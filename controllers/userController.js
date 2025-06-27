dede1c25f2dd1a5289d570cb519823a3624eb68c
Resya-Hasan <resyahasan0@gmail.com>
2025-06-27 14:45:32 +0700

    feat: login done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const { comparePassword } = require('../helpers/bcrypt');
+const { signToken } = require('../helpers/jwt');
 const { User } = require('../models');
 
 class UserController {
@@ -8,7 +10,39 @@ class UserController {
             console.log("Registering user:", req.body);
 
             res.status(201).json({
-                message: "User registered successfully",
+                message: "Registered successfully",
+            });
+        } catch(err) {
+            next(err)
+        }
+    }
+
+    static async login(req, res, next) {
+        try {
+            const { email, password } = req.body;
+
+            if (!email) {
+                throw { name: "BadRequest", message: "Email is required" };
+            }
+
+            if (!password) {
+                throw { name: "BadRequest", message: "Password is required" };
+            }
+
+            const user = await User.findOne({ where: { email } });
+
+            if (!user) {
+                return res.status(401).json({ message: "Invalid email or password" });
+            }
+
+            const isValid = comparePassword(password, user.password);
+            if (!isValid) {
+                return res.status(401).json({ message: "Invalid email or password" });
+            }
+
+            const token = signToken({ id: user.id });
+            res.status(200).json({
+                access_token: token,
             });
         } catch(err) {
             next(err)
